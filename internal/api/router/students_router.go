86876f1928911810d0983f7547b087781d36bd8e
rizkirisjad <127320212+rizkirisjad@users.noreply.github.com>
2025-10-27 17:12:50 +0700

    execs db handler and execs router

--- internal/api/router/students_router.go
+++ internal/api/router/students_router.go
@@ -1,1 +1,23 @@
 package router
+
+import (
+	"net/http"
+	"restapi/internal/api/handlers"
+)
+
+func studentsRouter() *http.ServeMux {
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /students", handlers.GetStudentsHandler)
+	mux.HandleFunc("POST /students", handlers.AddStudentsHandler)
+	mux.HandleFunc("PATCH /students", handlers.PatchStudentsHandler)
+	mux.HandleFunc("DELETE /students", handlers.DeleteStudentsHandler)
+
+	mux.HandleFunc("GET /students/{id}", handlers.GetStudentsHandler)
+	mux.HandleFunc("PUT /students/{id}", handlers.AddStudentsHandler)
+	mux.HandleFunc("PATCH /students/{id}", handlers.PatchStudentsHandler)
+	mux.HandleFunc("DELETE /students/{id}", handlers.DeleteStudentsHandler)
+
+	return mux
+}
